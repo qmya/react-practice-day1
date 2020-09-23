bb5a7ecaf1c295b423db68b67427a7eff5214033
qmya <qandeelgul90@gmail.com>
2020-09-22 21:21:16 -0400

    completed with adding calculator

--- src/App.js
+++ src/App.js
@@ -5,16 +5,9 @@ function App() {
   const [number1, setNumber1] = useState(0);
   const [number2, setNumber2] = useState(0);
   const [total1, setTotal1] = useState(number1 + number2);
-  const [total2, setTotal2] = useState(number1 * number2);
-  const [total3, setTotal3] = useState(number1 + number2);
-  const [total4, setTotal4] = useState(number1 + number2);
+  
 
-  // if(useState=number1 +number2){
-  //   console.log(total1);
-  // }
-  // else if(useState = number1 *number2){
-  //   total2;
-  // }
+  
 
   // function addThemTogather() {
   //   setTotal(number1 + number2);
@@ -42,11 +35,9 @@ function App() {
       
 
       <button onClick={() =>setTotal1(number1+number2) }>Add Them!</button>
-      <button onClick={()=>setTotal2(number1*number2)}>Multiply Them!</button>
-      <button onClick={() => setTotal3(number1-number2)}>Subtract them!</button>
-      <button onClick={() =>setTotal4(number1/number2)}>Divide them!</button>
+      
      
- 
+  <h1>{total1}</h1>
      
     
       
